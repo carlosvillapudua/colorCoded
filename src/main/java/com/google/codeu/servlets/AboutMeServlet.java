7919769ea448a2699abaa15b956e33c1744cadea
Timileyin Adebisi <35154844+Timileyin5@users.noreply.github.com>
2019-02-23 16:17:55 -0600

    Revert "updated about me page"

--- src/main/java/com/google/codeu/servlets/AboutMeServlet.java
+++ src/main/java/com/google/codeu/servlets/AboutMeServlet.java
@@ -1,65 +1,24 @@
 package com.google.codeu.servlets;
 
-import java.io.IOException;
+        import java.io.IOException;
 
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import com.google.appengine.api.users.UserService;
-import com.google.appengine.api.users.UserServiceFactory;
-import com.google.codeu.data.Datastore;
-
-/**
- * Handles fetching and saving user data.
- */
-@WebServlet("/about")
-public class AboutMeServlet extends HttpServlet {
-
-    private Datastore datastore;
-
-    @Override
-    public void init() {
-        datastore = new Datastore();
-    }
+        import javax.servlet.annotation.WebServlet;
+        import javax.servlet.http.HttpServlet;
+        import javax.servlet.http.HttpServletRequest;
+        import javax.servlet.http.HttpServletResponse;
 
     /**
-     * Responds with the "about me" section for a particular user.
+     * Responds with a hard-coded message for testing purposes.
      */
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws IOException {
+    @WebServlet("/about")
+    public class AboutMeServlet extends HttpServlet{
 
-        response.setContentType("text/html");
+        @Override
+        public void doGet(HttpServletRequest request, HttpServletResponse response)
+                throws IOException {
 
-        String user = request.getParameter("user");
-
-        if(user == null || user.equals("")) {
-            // Request is invalid, return empty response
-            return;
+            response.getOutputStream().println("hello world");
         }
-
-        String aboutMe = "This is " + user + "'s about me.";
-
-        response.getOutputStream().println(aboutMe);
     }
 
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws IOException {
 
-        UserService userService = UserServiceFactory.getUserService();
-        if (!userService.isUserLoggedIn()) {
-            response.sendRedirect("/index.html");
-            return;
-        }
-
-        String userEmail = userService.getCurrentUser().getEmail();
-
-        System.out.println("Saving about me for " + userEmail);
-        // TODO: save the data
-
-        response.sendRedirect("/user-page.html?user=" + userEmail);
-    }
-}
