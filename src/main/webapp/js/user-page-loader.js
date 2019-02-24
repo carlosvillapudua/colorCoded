19b449e38da8d284957dfa61f258b586ca1cc428
Timileyin <timade55@gmail.com>
2019-02-24 01:39:17 -0600

    updated user-page html and js to show message box

--- src/main/webapp/js/user-page-loader.js
+++ src/main/webapp/js/user-page-loader.js
@@ -43,7 +43,6 @@ function showMessageFormIfViewingSelf() {
           const messageForm = document.getElementById('message-form');
           messageForm.classList.remove('hidden');
         }
-        document.getElementById('about-me-form').classList.remove('hidden');
       });
 }
 
@@ -96,19 +95,4 @@ function buildUI() {
   setPageTitle();
   showMessageFormIfViewingSelf();
   fetchMessages();
-  fetchAboutMe();
-}
-function fetchAboutMe(){
-  const url = '/about?user=' + parameterUsername;
-  fetch(url).then((response) => {
-    return response.text();
-  }).then((aboutMe) => {
-    const aboutMeContainer = document.getElementById('about-me-container');
-    if(aboutMe == ''){
-      aboutMe = 'This user has not entered any information yet.';
-    }
-
-    aboutMeContainer.innerHTML = aboutMe;
-
-  });
 }
