376eb3f9d4937e421fb536d92c0c16af45cc7a5a
Nico <nicolasabarra@hotmail.com>
2019-03-25 02:45:40 -0700

    sentiment analysis. changes in message, messageservlet and datastore, and pom.

--- src/main/java/com/google/codeu/data/Message.java
+++ src/main/java/com/google/codeu/data/Message.java
@@ -30,22 +30,26 @@ public class Message {
 
   private String recipient;
 
+
+  private float sentimentScore;
+
   /**
    * Constructs a new {@link Message} posted by {@code user} with {@code text} content. Generates a
    * random ID and uses the current system time for the creation time.
    */
 
   //Edited constructors by Nicole (Step 2 Direct Messages)
-  public Message(String user, String text, String recipient) {
-  this(UUID.randomUUID(), user, text, System.currentTimeMillis(), recipient);
+  public Message(String user, String text, String recipient, float sentimentScore) {
+  this(UUID.randomUUID(), user, text, System.currentTimeMillis(), recipient, sentimentScore);
   }
 
-public Message(UUID id, String user, String text, long timestamp, String recipient) {
+public Message(UUID id, String user, String text, long timestamp, String recipient, float sentimentScore) {
   this.id = id;
   this.user = user;
   this.text = text;
   this.timestamp = timestamp;
   this.recipient = recipient;
+  this.sentimentScore = sentimentScore;
   }
 
   public UUID getId() {
@@ -68,4 +72,8 @@ public Message(UUID id, String user, String text, long timestamp, String recipie
   public String getRecipient(){
   return recipient;
 }
+
+  public float getSentimentScore(){
+    return sentimentScore;
+  }
 }
